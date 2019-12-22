879e0febfb8a6653caf091217611918f8230842d
Dan V <49368667+dvuk84@users.noreply.github.com>
2019-12-22 16:47:10 +0000

    Update variables.tf
    adding a map of allowed inbound ports

--- terraform-aws-kubernetes-cluster/variables.tf
+++ terraform-aws-kubernetes-cluster/variables.tf
@@ -6,3 +6,23 @@ variable "credentials" {}
 variable "subnet" {}
 variable "keyname" {}
 variable "keypath" {}
+variable "inbound_ports" {
+  default = [
+    {
+      from_port = "22",
+      to_port   = "22"
+    },
+    {
+      from_port = "80",
+      to_port   = "80"
+    },
+    {
+      from_port = "443",
+      to_port   = "443"
+    },
+    {
+      from_port = "6443",
+      to_port   = "6443"
+    }
+  ]
+}
